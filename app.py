4bb84f63726145005df5a25f4f3cdfc67c5152e1
QuanBlue <quannguyenthanh558@gmail.com>
2023-03-02 15:16:28 +0700

    fix: show image

--- app.py
+++ app.py
@@ -66,10 +66,7 @@ def main():
             # get chatbot response
             try:
                 message = call_openai(prompt)
-                # message = "\n\n#include <stdio.h>\n\nint main(void)\n{\n    printf(\"Hello, World!\\n\");\n    return 0;\n}".replace(
-                #     "\n", "<br>").replace("\"", "\`")
-                # message = "one<br>two<br>three"
-                # message = "![alt text](https: // upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/220px-The_Earth_seen_from_Apollo_17.jpg)"
+
             except Exception as e:
                 print("Error:", e)
                 if "Incorrect API key provided" in str(e):
@@ -98,6 +95,3 @@ def main():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, threaded=True, debug=True)
-
-
-# "\n\n<!DOCTYPE html>\n<html>\n<head>\n    <title>Web Commercial</title>\n    <style>\n        body {\n            background-color: #eee;\n            font-family: sans-serif;\n            font-size: 1.2em;\n        }\n\n        .commercial {\n            margin: auto;\n            width: 900px;\n            padding: 20px; \n            background-color: #fff; \n            border-radius: 5px; \n        }\n\n        h1 {\n            font-size: 2.5em; \n            font-weight: bold; \n            color: #444; \n        }\n\n        p {\n            margin: 10px 0; \n        }\n\n        .product-image {\n            float: left; \n            margin-right: 20px; \n        }\n\n        .product-description {\n            float: left; \n            width: 600px; \n        }\n\n        .product-price {\n            font-size: 1.5em; \n            font-weight: bold; \n            color: #009900; \n        }\n\n    </style>\n</head>\n<body>\n\n    <div class=\"commercial\">\n\n        <h1>Introducing the all new Product X!</h1>\n\n        <div class=\"product\">\n\n            <div class=\"product-image\">\n                <img src=\"product_x.jpg\" alt=\"Product X\" />  \n            </div>\n\n            <div class=\"product-description\">\n\n                <p>Product X is the latest innovation in technology that you won't want to miss out on! With features like...</p>\n\n                <ul>\n                    <li>Feature 1</li> \n                    <li>Feature 2</li> \n                    <li>Feature 3</li> \n                </ul>\n\n                <p class=\"product-price\">Only $99.99!</p>\n\n                <p><a href=\"#\">Order now!</a></p>\n\n            </div>\n\n        </div> <!-- end product -->\n\n    </div><!-- end commercial -->    \n    \n</body>  \n</html>"
